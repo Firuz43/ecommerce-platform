86ca54f3883627a9d32d497bf59e69ead75e4825
Firuz Chamarov <firuzchamarov@gmail.com>
2026-02-09 20:43:24 +0400

    change

---
+++ backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.backend.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.backend.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
