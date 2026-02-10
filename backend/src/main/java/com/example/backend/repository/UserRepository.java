d8d23a29675463f4681bdbf0adde0a43b02c4224
Firuz Chamarov <firuzchamarov@gmail.com>
2026-02-10 16:04:11 +0400

    changes

--- backend/src/main/java/com/example/backend/repository/UserRepository.java
+++ backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.example.backend.repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
